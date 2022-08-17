c1676c2542effc8f64b08a1aa8cb697be93dab1b
jaferIdrees <jaferll@gmail.com>
2022-08-17 18:13:47 +0300

    Apply required changes

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/books';
 
 function AddBookForm() {
+  const [bookData, createBook] = useState({});
+  const { titleEntry, authorEntry } = bookData;
+  const newBook = () => createBook((bookData) => {
+    const book = {
+      title: bookData.titleEntry,
+      author: bookData.authorEntry,
+      id: uuidv4(),
+    };
+    return book;
+  });
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addBook({
-      title: e.target.title.value,
-      author: e.target.author.value,
-    }));
+    dispatch(addBook(
+      newBook() || {
+        title: e.target.title.value,
+        author: e.target.author.value,
+        id: uuidv4(),
+      },
+    ));
+    e.target.reset();
   };
   return (
     <form onSubmit={handleSubmit}>
       <h2>ADD NEW BOOK</h2>
-      <input type="text" id="titleInput" required name="title" placeholder="Book title" />
-      <input type="text" id="authorInput" required name="author" placeholder="Author" />
+      <input type="text" id="titleInput" required name="title" placeholder="Book title" value={titleEntry} />
+      <input type="text" id="authorInput" required name="author" placeholder="Author" value={authorEntry} />
       <button type="submit">ADD BOOK</button>
     </form>
   );
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -5,12 +5,13 @@ import { checkStatus } from '../redux/categories/categories';
 function Categories() {
   const state = useSelector((state) => state.categoriesReducer);
   const dispatch = useDispatch();
+  const checkCategoriesStatus = () => dispatch(checkStatus());
   return (
     <div>
       <h2>{state}</h2>
       {' '}
 
-      <button type="button" onClick={() => dispatch(checkStatus())}>Check status</button>
+      <button type="button" onClick={checkCategoriesStatus}>Check status</button>
     </div>
   );
 }
