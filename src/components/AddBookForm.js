e7191ba95f2bc9cd0664aae9de97dfd5be3e2386
jaferIdrees <jaferll@gmail.com>
2022-08-17 20:05:44 +0300

    Apply required changes

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -4,33 +4,27 @@ import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/books';
 
 function AddBookForm() {
-  const [bookData, createBook] = useState({});
-  const { titleEntry, authorEntry } = bookData;
-  const newBook = () => createBook((bookData) => {
-    const book = {
-      title: bookData.titleEntry,
-      author: bookData.authorEntry,
-      id: uuidv4(),
-    };
-    return book;
-  });
+  const [bookData, createBook] = useState({ title: '', author: '' });
+  // const { titleEntry, authorEntry } = bookData;
+  const newBook = (e) => (
+    createBook({
+      ...bookData,
+      [e.target.name]: e.target.value,
+    }));
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addBook(
-      newBook() || {
-        title: e.target.title.value,
-        author: e.target.author.value,
-        id: uuidv4(),
-      },
-    ));
+    dispatch(addBook({
+      ...bookData,
+      id: uuidv4(),
+    }));
     e.target.reset();
   };
   return (
     <form onSubmit={handleSubmit}>
       <h2>ADD NEW BOOK</h2>
-      <input type="text" id="titleInput" required name="title" placeholder="Book title" value={titleEntry} />
-      <input type="text" id="authorInput" required name="author" placeholder="Author" value={authorEntry} />
+      <input type="text" id="titleInput" required name="title" placeholder="Book title" onChange={newBook} />
+      <input type="text" id="authorInput" required name="author" placeholder="Author" onChange={newBook} />
       <button type="submit">ADD BOOK</button>
     </form>
   );
